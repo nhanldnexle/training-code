ba8575bcab39f3e2c7280e080534087628c343b4
Nhanld <ledinhnhan13198@gmail.com>
2024-07-09 16:06:39 +0700

    a

---
+++ src/test/java/TestSelenium.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestSelenium {
+
+    public static void main(String[] args) {
+
+        //Khởi tạo browser với Chrome
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Mở trang anhtester.com
+        driver.get("https://beahub-web-v1005.devcaprover.nexlesoft.com/");
+
+        //Click nút Login
+        //driver.findElement(By.xpath("//a[@id='btn-login']")).click();
+
+        //Tắt browser
+        //driver.quit();
+    }
+
+}
